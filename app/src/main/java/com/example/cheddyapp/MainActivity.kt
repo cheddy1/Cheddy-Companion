b67a4c06ff9ff5236cfb3034302e37564eba3788
Christian <elcman965@gmail.com>
2021-12-23 18:20:26 -0600

    implemented some tab fixes

--- app/src/main/java/com/example/cheddyapp/MainActivity.kt
+++ app/src/main/java/com/example/cheddyapp/MainActivity.kt
@@ -2,14 +2,8 @@ package com.example.cheddyapp
 
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.example.cheddyapp.ui.main.SectionsPagerAdapter
+import com.example.cheddyapp.adapter.ViewPagerAdapter
 import com.example.cheddyapp.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
@@ -19,10 +13,16 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Call
 import retrofit2.converter.gson.GsonConverterFactory
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 private const val BASE_URL = "https://api.tdameritrade.com/v1/marketdata/"
 private const val TAG = "MainActivity"
+private val tabArray = arrayOf(
+    "Roth IRA",
+    "Brokerage"
+)
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var curTickerData: JsonObject
@@ -34,34 +34,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabArray[position]
+        }.attach()
 
         val gson = GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").create()
-        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).build()
-
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson)).build()
         val stockService = retrofit.create(StockService::class.java)
         val ticker = Ticker("TWTR")
-        stockService.getSingleCurPrice(BASE_URL+ticker.ticker+"/quotes?apikey=").enqueue(object: Callback<JsonObject> {
-            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
-                Log.i(TAG, "onResponse $response")
-                val tickerData = response.body()
-                if (tickerData == null){
-                    Log.w(TAG, "Did not recieve valid response body")
-                    return
+        stockService.getSingleCurPrice(BASE_URL + ticker.ticker + "/quotes?apikey=")
+            .enqueue(object : Callback<JsonObject> {
+                override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                    Log.i(TAG, "onResponse $response")
+                    val tickerData = response.body()
+                    if (tickerData == null) {
+                        Log.w(TAG, "Did not recieve valid response body")
+                        return
+                    }
+                    val values =
+                        Gson().fromJson(tickerData.get(ticker.ticker), StockData::class.java)
+                    Log.i(TAG, "onResponse $values")
                 }
-                val values = Gson().fromJson(tickerData.get(ticker.ticker), StockData::class.java)
-                Log.i(TAG, "onResponse $values")
-            }
 
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                Log.e(TAG, "onFailure $t")
-            }
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.e(TAG, "onFailure $t")
+                }
 
-        })
+            })
     }
 }
