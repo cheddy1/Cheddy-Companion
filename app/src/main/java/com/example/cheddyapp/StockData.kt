00c3c8c8ccb8cf7448ddaa6e887a5a982cb502c1
Christian <elcman965@gmail.com>
2021-12-24 14:20:47 -0600

    removed unneeded data class

--- app/src/main/java/com/example/cheddyapp/StockData.kt
+++ app/src/main/java/com/example/cheddyapp/StockData.kt
@@ -1,10 +1,6 @@
 package com.example.cheddyapp
-import com.google.gson.JsonObject
-import com.google.gson.annotations.SerializedName
 
-data class Ticker   (
-    val ticker: String
-    )
+import com.google.gson.annotations.SerializedName
 
 data class StockData    (
     @SerializedName("lastPrice") val lastPrice: Double,
