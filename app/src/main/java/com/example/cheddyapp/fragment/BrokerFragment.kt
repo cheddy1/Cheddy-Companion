872b7bf1a63f1ecf41cdb2d3629dbff4213662bc
Christian <elcman965@gmail.com>
2021-12-25 18:18:22 -0600

    implemented general stockHandler

--- app/src/main/java/com/example/cheddyapp/fragment/BrokerFragment.kt
+++ app/src/main/java/com/example/cheddyapp/fragment/BrokerFragment.kt
@@ -5,20 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.cheddyapp.*
 import com.example.cheddyapp.databinding.FragmentBrokeBinding
+
 
 class BrokerFragment : Fragment() {
     private var _binding: FragmentBrokeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var swipeContainer: SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentBrokeBinding.inflate(inflater, container, false)
+        swipeContainer = requireActivity().findViewById(R.id.swipeContainer)
+        val stockDataHandler = StockDataHandler("broker", this.activity, binding.brokerConstraint, swipeContainer)
+        swipeContainer.setOnRefreshListener {
+            stockDataHandler.fetchStockData()
+        }
+        stockDataHandler.fetchStockData()
         return binding.root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
--- app/src/main/java/com/example/cheddyapp/fragment/RothFragment.kt
+++ app/src/main/java/com/example/cheddyapp/fragment/RothFragment.kt
@@ -5,20 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.cheddyapp.*
 import com.example.cheddyapp.databinding.FragmentRothBinding
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
 
 class RothFragment : Fragment() {
     private var _binding: FragmentRothBinding? = null
     private val binding get() = _binding!!
+    private lateinit var swipeContainer: SwipeRefreshLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentRothBinding.inflate(inflater, container, false)
+        swipeContainer = requireActivity().findViewById(R.id.swipeContainer)
+        val stockDataHandler = StockDataHandler("roth", this.activity, binding.rothConstraint, swipeContainer)
+        swipeContainer.setOnRefreshListener {
+            stockDataHandler.fetchStockData()
+        }
+        stockDataHandler.fetchStockData()
         return binding.root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
