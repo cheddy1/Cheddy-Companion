4045be5f2616e52f3d083c8014976d626cfea290
Christian <elcman965@gmail.com>
2022-01-19 23:16:11 -0600

    adding Dao needed for sqlite

--- app/src/main/java/com/example/cheddyapp/StockService.kt
+++ app/src/main/java/com/example/cheddyapp/StockService.kt
@@ -1,11 +1,29 @@
 package com.example.cheddyapp
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Url
 
 interface StockService  {
+
     @GET
     fun getSingleCurPrice(@Url url: String): Call<JsonObject>
+}
+
+@Dao
+interface UserDataDao {
+
+    @Query("SELECT * FROM UserTickerData WHERE accountName = :accountName")
+    fun getData(accountName: String): List<UserTickerData>
+
+    @Insert
+    fun insertData(data: UserTickerData)
+
+    @Update
+    fun updateData(data: UserTickerData)
 }
